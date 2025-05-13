7e51c02242c8a1c9f1ae66019a3dd303f46a1d4e
sheaba <abdallaayman140@gmail.com>
2025-05-13 17:39:48 +0300

    addig filtering and pagination to views.py

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -5,7 +5,7 @@ from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework import status
 from rest_framework.renderers import TemplateHTMLRenderer
-
+from django.core.paginator import Paginator, EmptyPage
 
 
 @api_view(['GET','POST'])
@@ -14,10 +14,25 @@ def menu_items(request):
         items = MenuItem.objects.select_related('category').all()
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        perpage = request.query_params.get('perpage', default=2)
+        page = request.query_params.get('page', default=1)
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price=to_price)
+        if search:
+            items = items.filter(title__contains=search)
+        if ordering:
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+
+        paginator = Paginator(items, per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items = []
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
     if (request.method == 'POST'):
